798f01df0d40259ee9a70f133e55fc07fa41c83e
SonamKul9527 <kulsonam28@gmail.com>
2022-12-15 00:29:01 +0530

    Adding 3 models code

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,12 @@
 
 
 print("Adding new file")
+
+def model1() :
+    print("Adding code for ML model 1")
+
+def model2() :
+    print("Adding code for ML model 2")
+
+def model3() :
+    print("Adding code for ML model 3")
