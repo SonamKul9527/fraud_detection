be54ed0705d564be10b2cd01bc15337ca9b88326
SonamKul9527 <kulsonam28@gmail.com>
2022-12-15 00:02:12 +0530

    Revert "Adding sum method"
    This reverts commit a568a997d484e21a787b859dd18054b2f411f549.

--- data_processing.py
+++ data_processing.py
@@ -1,13 +1,2 @@
 
 print("Adding all the data processing code to github repo")
-
-a = 27
-b = 28
-
-## sum(a,b)
-
-def sum(a,b) :
-    c = a+b
-    return a
-
-print("Adding new incorrect function")
