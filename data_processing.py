a568a997d484e21a787b859dd18054b2f411f549
SonamKul9527 <kulsonam28@gmail.com>
2022-12-14 23:59:42 +0530

    Adding sum method

--- data_processing.py
+++ data_processing.py
@@ -1,2 +1,13 @@
 
 print("Adding all the data processing code to github repo")
+
+a = 27
+b = 28
+
+## sum(a,b)
+
+def sum(a,b) :
+    c = a+b
+    return a
+
+print("Adding new incorrect function")
